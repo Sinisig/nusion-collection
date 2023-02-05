7d56dcd35d4140027fe968e778c7d22674be253f
Sinisig <Shacklebird1448@gmail.com>
2023-02-04 22:23:31 -0500

    Add even MORE boilerplate, implementation of DllMain is ready

--- lib/sys/src/os/windows/macros.rs
+++ lib/sys/src/os/windows/macros.rs
@@ -2,8 +2,14 @@
 
 #[macro_export]
 macro_rules! build_slib_entry_os {
-   ($entry:ident, $init:ident) => {
+   ($entry:ident, default)    => {
 
-   }
+   };
+   ($entry:ident, result_st)  => {
+
+   };
+   ($entry:ident, result_dy)  => {
+
+   };
 }
 
--- lib/sys/src/runtime.rs
+++ lib/sys/src/runtime.rs
@@ -16,9 +16,26 @@ where F: FnOnce(),
 }
 
 /// Starts main with a
+/// Result<(), E: std::error::Error>
+/// return type.
+pub fn run_main_result_static<F, E>(
+   entry : F,
+) -> i32
+where F: FnOnce() -> Result<(), E>,
+      E: std::error::Error,
+{
+   if let Err(err) = entry() {
+      eprintln!("Error: {}", err.to_string());
+      return EXIT_FAILURE;
+   }
+
+   return EXIT_SUCCESS;
+}
+
+/// Starts main with a
 /// Result<(), Box<dyn std::error::Error>>
 /// return type.
-pub fn run_main_result<F>(
+pub fn run_main_result_dynamic<F>(
    entry : F,
 ) -> i32
 where F: FnOnce() -> Result<(), Box<dyn std::error::Error>>
