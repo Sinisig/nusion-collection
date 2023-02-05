516a1a86e966c6e88dedc77ef9f379f8b04c2e23
Sinisig <Shacklebird1448@gmail.com>
2023-02-04 21:09:46 -0500

    Improve nusion entry macro, now need to write sys entrypoint generator

--- lib/proc/src/lib.rs
+++ lib/proc/src/lib.rs
@@ -6,15 +6,15 @@
 //! but instead include nusion as a dependency, as
 //! nusion re-exports all macros in this crate.
 
-extern crate proc_macro;
-
 /// Builds a shared library entrypoint
 /// using the attached function item.
 /// The function signature should be
 /// the same as main's signature.
 /// This should only ever be used on
-/// a single function.
+/// a single function inside a dynamic
+/// library crate.
 #[proc_macro_attribute]
+#[proc_macro_error::proc_macro_error]
 pub fn entry(
    _     : proc_macro::TokenStream,
    item  : proc_macro::TokenStream,
@@ -22,9 +22,33 @@ pub fn entry(
    // Store the user function for later
    let user_func = item.clone();
 
+   // Parse item into function signature
+   let signature = match syn::parse_macro_input!(item as syn::Item) {
+      syn::Item::Fn(func) => func.sig,
+      _  => proc_macro_error::abort_call_site!(
+         "Entrypoint item is not a function";
+
+         help = "Change item type to a main-like function";
+      ),
+   };
+
+   // Verify function arguments
+   if signature.inputs.empty_or_trailing() == false {
+      proc_macro_error::emit_error!(
+         proc_macro_error::SpanRange::call_site(), // TODO: Better span
+         "Entrypoint function has non-zero argument count";
+
+         help = "Remove all input arguments";
+      );
+   }
+
+   // TODO: Verify function return type (output) in here
+   // instead of evaluating in the sys macro for better
+   // error messages.
+
    // Format entry point constrution
    let slib_entry : proc_macro::TokenStream = format!(r"
-
+     
    ").parse().unwrap();
 
    // Insert entrypoint before the user function and return
