5ce57cbf899379fe2f8a101519a9118592b2dd2c
Sinisig <Shacklebird1448@gmail.com>
2023-02-11 21:31:52 -0500

    Add new code to DRG implementation

--- game/drg/src/lib.rs
+++ game/drg/src/lib.rs
@@ -1,6 +1,14 @@
 #[nusion::entry]
 pub fn entry() -> Result<(), Box<dyn std::error::Error>> {
-   nusion::Environment::get_mut().console_mut().set_title(
+   const GAME_PROCESS_NAME : &str = "FSD-Win64-Shipping.exe";
+
+   let mut env = nusion::Environment::try_get_mut()?;
+
+   if env.process().executable_file_name() != GAME_PROCESS_NAME {
+      return Err(Box::new(MainError::WrongProcess));
+   }
+
+   env.console_mut().set_title(
       "Nusion for Deep Rock Galactic by Sinisig",
    )?;
 
@@ -8,5 +16,25 @@ pub fn entry() -> Result<(), Box<dyn std::error::Error>> {
    std::thread::sleep(std::time::Duration::from_secs(5));
 
    return Ok(());
+}
+
+#[derive(Debug)]
+enum MainError {
+   WrongProcess,
+}
+
+impl std::fmt::Display for MainError {
+   fn fmt(
+      & self,
+      stream : & mut std::fmt::Formatter<'_>,
+   ) -> std::fmt::Result {
+      return write!(stream, "{}", match self {
+         Self::WrongProcess
+            => "Process is not Deep Rock Galactic",
+      });
+   }
+}
+
+impl std::error::Error for MainError {
 }
 
