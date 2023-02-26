8a756de81e2ca16b5ed23e531004bbeccdbbcccf
Sinisig <Shacklebird1448@gmail.com>
2023-02-25 22:41:59 -0500

    Update temporary checksum algorithm to not panic

--- lib/main/src/patch.rs
+++ lib/main/src/patch.rs
@@ -314,7 +314,9 @@ impl Checksum {
       data  : & [u8],
    ) -> Self {
       // TODO: Better checksum algorithm
-      let checksum = data.iter().map(|b| *b as u32).sum();
+      let checksum = data.iter().fold(0, |sum : u32, b : & u8| {
+         sum.overflowing_add(*b as u32).0
+      });
 
       return Self{
          checksum : checksum,
