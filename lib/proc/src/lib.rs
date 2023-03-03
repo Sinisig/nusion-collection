85cc6c7afd46900ba9891b7331822154f38c9af1
Sinisig <Shacklebird1448@gmail.com>
2023-03-03 17:04:25 -0500

    Refactored hook macro

--- lib/proc/src/lib.rs
+++ lib/proc/src/lib.rs
@@ -193,128 +193,6 @@ pub fn entry(
 // ATTRIBUTE MACRO DEFINITION - hook //
 ///////////////////////////////////////
 
-struct HookAttributes {
-   asm_template   : syn::LitStr,
-   fn_ptr_ident   : syn::Ident,
-}
-
-impl syn::parse::Parse for HookAttributes {
-   fn parse(
-      input : syn::parse::ParseStream<'_>,
-   ) -> syn::parse::Result<Self> {
-      let asm_template  = input.parse()?;       // ASM Template
-      input.parse::<syn::Token![,]>()?;         // Required comma separator
-      let fn_ptr_ident  = input.parse()?;       // ASM Fn Ptr Identifier
-      input.parse::<Option<syn::Token![,]>>()?; // Optional trailing comma
-
-      return Ok(Self{
-         asm_template   : asm_template,
-         fn_ptr_ident   : fn_ptr_ident,
-      });
-   }
-}
-
-#[derive(Clone, Debug)]
-enum HookTemplateArgument {
-   TargetHookLabel,
-}
-
-impl std::str::FromStr for HookTemplateArgument {
-   type Err = ();
-
-   fn from_str<'l>(
-      s : &'l str,
-   ) -> Result<Self, Self::Err> {
-      use std::collections::hash_map::HashMap;
-
-      lazy_static::lazy_static! {
-         static ref ARGUMENT_MAP : HashMap<&'static str, HookTemplateArgument> = [
-            // Add new arguments here!
-            ("hook", HookTemplateArgument::TargetHookLabel)
-         ].iter().cloned().collect();
-      }
-
-      return ARGUMENT_MAP.get(s).ok_or(()).map(|a| a.clone());
-   }
-}
-
-#[derive(Debug)]
-struct HookTemplateReplacer<'s> {
-   label_target   : &'s str,
-}
-
-impl<'s> regex::Replacer for HookTemplateReplacer<'s> {
-   fn replace_append(
-      & mut self,
-      caps  : & regex::Captures<'_>,
-      dst   : & mut String,
-   ) {
-      for cap in caps.iter() {
-         // Get the capture as a string
-         let cap = match cap {
-            Some(cap)   => cap.as_str(),
-            None        => break,
-         };
-         
-         // Strip out the surrounding curly brackets
-         let cap = &cap[1..cap.len()-1];
-
-         // Try to parse as an argument
-         let arg = match cap.parse::<HookTemplateArgument>() {
-            Ok(a)    => a,
-            Err(_)   => panic!("invalid template argument \"{cap}\""),
-         };
-
-         // Write the new substituted text
-         dst.push_str(match arg {
-            HookTemplateArgument::TargetHookLabel
-               => self.label_target,
-         });
-      }
-
-      return;
-   }
-}
-
-/// Internal helper, formats an input
-/// ASM template to one ready to be
-/// passed to global_asm!()
-fn hook_format_asm_template(
-   template       : & str,
-   label_assembly : & str,
-   label_target   : & str,
-) -> String {
-   // Regex for finding and replacing
-   // template arguments
-   lazy_static::lazy_static! {
-      static ref TEMPLATE_SEARCHER : regex::Regex = regex::Regex::new(r"(?x)
-         \{[A-Za-z0-9]*?\}
-      ").unwrap();
-   }
-
-   // Search for and iterate over every
-   // template argument
-   let template = TEMPLATE_SEARCHER.replace_all(
-      template,
-      HookTemplateReplacer{
-         label_target   : label_target,
-      },
-   );
-
-   // Convert to an owned string
-   let template = template.into_owned();
-
-   // Append the prefix stuff for
-   // the function definition
-   let template = format!("
-      {}:
-      {}
-   ", label_assembly, template);
-
-   // Return the formatted ASM template
-   return template;
-}
-
 /// Constructs an associated assembly
 /// intermediate subroutine to call
 /// the attached function from a hook
@@ -382,119 +260,266 @@ pub fn hook(
    attr  : proc_macro::TokenStream,
    item  : proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+   // Parse item as a function
+   let hook = syn::parse_macro_input!(item as syn::ItemFn);
+
    // Parse attributes as arguments
-   let args = syn::parse_macro_input!(attr as HookAttributes);
+   let args = syn::parse_macro_input!(attr as HookArguments);
 
-   // Make a copy of the hook function
-   let hook_func = item.clone();
+   // Generate a unique identifier for the function
+   use core::hash::{Hash, Hasher};
+   let mut hasher = HookHasher::new();
+   hook.hash(& mut hasher);
 
-   // Get the function signature to copy
-   // for the intermediary
-   let hook_func_signature = syn::parse_macro_input!(item as syn::ItemFn);
-   let hook_func_signature = hook_func_signature.sig;
+   // Store the unique identifier as a u64
+   let uuid = hasher.finish();
 
-   // Store the identifier and create
-   // a list of the arguments without
-   // the type information
-   let hook_func_ident        = &hook_func_signature.ident;
-   let mut hook_func_inputs   = syn::punctuated::Punctuated::<
+   // Create identifiers for ASM and intermediary hooks
+   const IDENT_PREFIX : &'static str = "__nusion_hook";
+
+   let ident_asm = quote::format_ident!(
+      "{IDENT_PREFIX}_asm_{:X}_{}",
+      uuid,
+      hook.sig.ident,
+   );
+   let ident_int = quote::format_ident!(
+      "{IDENT_PREFIX}_int_{:X}_{}",
+      uuid,
+      hook.sig.ident,
+   );
+
+   // Substitute in template arguments
+   let asm = args.substitute_arguments(
+      & ident_asm,
+      & ident_int,
+   );
+
+   // Store input and output for hook
+   let input_hook    = &hook.sig.inputs;
+   let output_hook   = &hook.sig.output;
+
+   // Convert the hook input to a list of
+   // arguments to call the hook
+   let mut input_call_hook = syn::punctuated::Punctuated::<
       syn::Ident, syn::token::Comma,
    >::new();
-   for p in &hook_func_signature.inputs {
-      // Isolate the identifier for the arg
+   
+   for p in hook.sig.inputs.iter() {
+      // Isolate the identifier for the argument
       let p = match p {
          syn::FnArg::Typed(p) => p,
-         _                    => proc_macro_error::abort_call_site!(""),
+         _                    => {
+            proc_macro_error::emit_call_site_error!(
+               "hook function argument has no type",
+            );
+            continue;
+         },
       };
       let p = match &*p.pat {
          syn::Pat::Ident(p)   => p,
-         _                    => proc_macro_error::abort_call_site!(""),
+         _                    => {
+            proc_macro_error::emit_call_site_error!(
+               "hook function argument has no identifier",
+            );
+            continue;
+         },
       };
       let p = &p.ident;
 
-      // Add the identifier to the list
-      hook_func_inputs.push_value(p.clone());
+      // Add the identifier to the argument list
+      input_call_hook.push(p.clone());
    }
 
-   // Create a checksum of the entire
-   // hook function for appending to
-   // labels.  This will help prevent
-   // clashes with simmilarly named
-   // items.
-   let hook_func_chksm = crc::Crc::<u64>::new(
-      &crc::CRC_64_WE,
-   ).checksum(&hook_func.to_string().bytes().collect::<Vec<u8>>());
+   // Get miscellaneous references to prepare
+   // for construction of the code
+   let ident_hook_ptr   = &args.ptr_ident;
+   let ident_hook       = &hook.sig.ident;
 
-   // Generate identifiers for ASM hook
-   // and extern "C" intermediate
-   let label_hll = quote::format_ident!(
-      "__nusion_hook_hll_{}_{}",
-      format!("{:#016X}", hook_func_chksm)[2..],
-      hook_func_ident,
-   );
-   let label_asm = quote::format_ident!(
-      "__nusion_hook_asm_{}_{}",
-      format!("{:#016X}", hook_func_chksm)[2..],
-      hook_func_ident,
-   );
+   // Finally, put everything together
+   // and construct the syntax for the
+   // ASM hook, intermediary, and hook
+   return proc_macro::TokenStream::from(quote::quote!{
+      // Assembly hook
+      core::arch::global_asm!(#asm);
 
-   // Get the identifier for the constant
-   // function pointer for the ASM hook
-   let ident_asm_ptr = args.fn_ptr_ident;
-
-   // Convert the stored ASM template to
-   // a sanitized string which doesn't
-   // include the quotes from the string
-   // literal
-   let asm_template = args.asm_template.token().to_string();
-   let asm_template = &asm_template[1..asm_template.len() - 1];
-
-   // Format the ASM template to the
-   // final one passed to global_asm()!
-   let asm_template = hook_format_asm_template(
-      asm_template,
-      &label_asm.to_string(),
-      &label_hll.to_string(),
-   );
-
-   // Create the function signature for
-   // the intermediate
-   let mut inter_sig = hook_func_signature.clone();
-   inter_sig.ident   = syn::Ident::new(
-      &label_hll.to_string(),
-      proc_macro2::Span::call_site(),
-   );
-
-   // Put everything together and generate
-   // the hook syntax
-   let hook = proc_macro::TokenStream::from(quote::quote!{
-      // Assemble the intermediary hook
-      core::arch::global_asm!(#asm_template);
-
-      // Declaration for our ASM hook
+      // Declaration to the assembly hook
       extern "C" {
-         pub fn #label_asm();
+         #[no_mangle]
+         #[allow(non_snake_case)]
+         pub fn #ident_asm();
       }
 
-      // Constant pointer to the ASM hook
-      const #ident_asm_ptr
-         : * const core::ffi::c_void
-         = #label_asm as * const core::ffi::c_void;
+      // Void pointer to the assembly hook
+      const #ident_hook_ptr : * const core::ffi::c_void
+         = #ident_asm as * const core::ffi::c_void;
 
-      // The intermediate function for calling
-      // the Rust code
+      // Intermediate for calling the user's hook
       #[no_mangle]
-      pub extern "C" #inter_sig {
-         return #hook_func_ident(#hook_func_inputs);
+      #[allow(non_snake_case)]
+      pub extern "C" fn #ident_int(
+         #input_hook
+      ) #output_hook {
+         return #ident_hook(#input_call_hook);
       }
+
+      // User's hook function
+      #hook
    });
-   
-   // Combine the hook function with the
-   // generated syntax and return the new
-   // syntax
-   let mut output = proc_macro::TokenStream::new();
-   output.extend(hook);
-   output.extend(hook_func);
-   return output;
+}
+
+#[derive(Debug)]
+struct HookArguments {
+   pub asm_template  : syn::LitStr,
+   pub ptr_ident     : syn::Ident,
+}
+
+impl HookArguments {
+   pub fn substitute_arguments(
+      & self,
+      identifier_asm : & syn::Ident,
+      identifier_int : & syn::Ident,
+   ) -> String {
+      lazy_static::lazy_static! {
+         static ref FIND_ARGUMENTS : regex::Regex = regex::Regex::new(
+            r"\{[[:alpha:]]*?\}",
+         ).unwrap();
+      }
+      let mut output = String::new();
+
+      // Construct subroutine label
+      output.push_str(&format!(
+         "{identifier_asm}:\n",
+      ));
+
+      // Substitute template arguments
+      output.push_str(&FIND_ARGUMENTS.replace(
+         &self.asm_template.value(),
+         HookAsmParser{
+            identifier_int : identifier_int,
+         },
+      ));
+
+      // Return formatted ASM
+      return output;
+   }
+}
+
+impl syn::parse::Parse for HookArguments {
+   fn parse(
+      input : syn::parse::ParseStream<'_>,
+   ) -> syn::parse::Result<Self> {
+      // Required - String literal containing ASM template
+      let asm_template = input.parse::<syn::LitStr>()?;
+
+      // Required - Comma separating ASM template and identifier
+      input.parse::<syn::Token![,]>()?;
+
+      // Required - Identifier for the ASM pointer constant
+      let ptr_ident = input.parse::<syn::Ident>()?;
+
+      // Optional - Trailing comma after the identifier
+      input.parse::<Option<syn::Token![,]>>()?;
+
+      // Create Arguments struct and return
+      return Ok(Self{
+         asm_template   : asm_template,
+         ptr_ident      : ptr_ident,
+      });
+   }
+}
+
+#[derive(Debug)]
+struct HookAsmParser<'s> {
+   pub identifier_int : &'s syn::Ident,
+}
+
+impl<'s> regex::Replacer for HookAsmParser<'s> {
+   fn replace_append(
+      & mut self,
+      caps  : & regex::Captures<'_>,
+      dst   : & mut String,
+   ) {
+      for cap in caps.iter() {
+         // Get the capture as a string
+         let cap = match cap {
+            Some(cap)   => cap.as_str(),
+            None        => break,
+         };
+
+         // Strip out surrounding curly brackets
+         let cap = &cap[1..cap.len()-1];
+
+         // Try to parse as an argument
+         let arg = match cap.parse::<HookArgumentType>() {
+            Ok(arg)  => arg,
+            Err(_)   => panic!("invalid template argument \"{cap}\""),
+         };
+
+         // Stringify the actual value for the argument
+         let arg = match arg {
+            HookArgumentType::IdentifierIntermediate
+               => self.identifier_int.to_string(),
+         };
+
+         // Append the substituted string to the buffer
+         dst.push_str(&arg);
+      }
+      return;
+   }
+}
+
+#[derive(Clone, Debug)]
+enum HookArgumentType {
+   IdentifierIntermediate,
+}
+
+impl std::str::FromStr for HookArgumentType {
+   type Err = ();
+
+   fn from_str(
+      s : & str,
+   ) -> Result<Self, Self::Err> {
+      use std::collections::hash_map::HashMap;
+
+      // Hash map for argument types
+      lazy_static::lazy_static! {
+         static ref ARGUMENT_MAP : HashMap<&'static str, HookArgumentType> = [
+            ("hook", HookArgumentType::IdentifierIntermediate),
+         ].iter().cloned().collect();
+      }
+
+      return ARGUMENT_MAP.get(s).ok_or(()).map(|a| a.clone());
+   }
+}
+
+#[derive(Debug)]
+struct HookHasher {
+   crc : u64,
+}
+
+impl HookHasher {
+   pub fn new() -> Self {
+      return Self{
+         crc : 0,
+      };
+   }
+}
+
+impl core::hash::Hasher for HookHasher {
+   fn finish(
+      & self,
+   ) -> u64 {
+      return self.crc;
+   }
+
+   fn write(
+      & mut self,
+      bytes : & [u8],
+   ) {
+      self.crc = crc::Crc::<u64>::new(
+         & crc::CRC_64_WE,
+      ).checksum(bytes);
+      return;
+   }
 }
 
