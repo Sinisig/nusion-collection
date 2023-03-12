001bb433a8ee2990c138f138626717bd1d0afe54
Sinisig <Shacklebird1448@gmail.com>
2023-03-12 16:05:31 -0400

    Clean up game/drg, now ready to start adding fun stuff

--- game/drg/src/exec.rs
+++ game/drg/src/exec.rs
@@ -2,32 +2,49 @@
 //! inside of the game's main
 //! thread.
 
-/// Stores various information about the
-/// state of the mod in the loop.
-pub struct LoopState {
-   pub test_counter : u32,
+//////////////////////
+// TYPE DEFINITIONS //
+//////////////////////
+
+/// Stores information about the main
+/// loop and executes the main loop.
+pub struct MainLoop {
+   test_counter : u32,
 }
 
-impl LoopState {
-   pub const fn new() -> Self {
+/// <code>Result</code> type returned by
+/// <code>MainLoop::execute</code> where
+/// the <code>Ok</code> variant signifies
+/// whether to keep executing or not and
+/// the <code>Err</code> variant implies
+/// to stop looping and return the error
+/// code.
+pub type Result = std::result::Result<bool, Box<dyn std::error::Error + Send>>;
+
+////////////////////////
+// METHODS - MainLoop //
+////////////////////////
+
+impl MainLoop {
+   // Initializes a new main loop.
+   pub fn init(
+   ) -> Self {
       return Self{
          test_counter   : 0,
       };
    }
-}
 
-/// Ok variant of the return type
-/// signifies whether the loop should
-/// keep executing or not.  The Err
-/// variant will return the error to
-/// the caller and implicitly stop
-/// looping.
-pub fn main_loop(
-   state : & mut LoopState,
-) -> Result<bool, Box<dyn std::error::Error + Send>> {
-   state.test_counter += 1;
-   println!("Executed loop! {}", state.test_counter);
+   // Executes one iteration of the main loop.
+   // This should only ever run on the same
+   // thread as the game.
+   pub fn execute(
+      & mut self,
+   ) -> Result {
+      println!("Main loop test counter: {}", self.test_counter);
 
-   return Ok(state.test_counter < 500);
+      self.test_counter += 1;
+
+      return Ok(self.test_counter <= 500);
+   }
 }
 
--- game/drg/src/init.rs
+++ game/drg/src/init.rs
@@ -4,32 +4,32 @@
 
 use nusion_lib::patch::Patch;
 
+//////////////////////
+// TYPE DEFINITIONS //
+//////////////////////
+
 /// Struct which stores the last return
 /// code from the main loop in a more
 /// friendly way for synchronization.
 struct LoopStatus{
+   main_loop      : Option<crate::exec::MainLoop>,
    should_execute : bool,
    err_code       : Option<Box<dyn std::error::Error + Send>>,
 }
 
-// Stores the loop state and is
-// passed by reference on each
-// loop execution.  This is only
-// actually used inside the loop,
-// so no need to synchronize.
-static mut LOOP_STATE
-   : crate::exec::LoopState
-   = crate::exec::LoopState::new();
+////////////////////////////////////
+// GLOBAL VARIABLES AND CONSTANTS //
+////////////////////////////////////
 
-// Stores the returned code from the
-// main loop.  This DOES require
-// synchronization.  This is also used
-// as a mutex lock for knowing if we
-// should execute the loop or not.
+// Stores the current state of the main loop.
+// This requires synchronization because the
+// returned information from <code>execute</code>
+// is accessed accross threads.
 lazy_static::lazy_static!{
-static ref LOOP_RETURN_CODE
+static ref LOOP_STATUS
    : std::sync::Mutex<LoopStatus>
    = std::sync::Mutex::new(LoopStatus{
+      main_loop      : Some(crate::exec::MainLoop::init()),
       should_execute : true,
       err_code       : None,
    });
@@ -39,68 +39,76 @@ static ref LOOP_RETURN_CODE
 // We have to be careful to minimize
 // the chance of a race condition since
 // this is applied asynchronously.
-const HOOK_LOOP : nusion_lib::patch::writer::Hook<std::ops::Range<usize>> = nusion_lib::patch::writer::Hook{
-   memory_offset_range  : 0x8241BC..0x8241CD,
-   checksum             : nusion_lib::patch::Checksum::from(0xF7946268),
-   hook                 : nusion_lib::hook!("
-      // Store volatiles and align stack
-      push  rax
+const LOOP_HOOK
+   : nusion_lib::patch::writer::Hook<std::ops::Range<usize>>
+   = nusion_lib::patch::writer::Hook{
+      memory_offset_range  : 0x8241BC..0x8241CD,
+      checksum             : nusion_lib::patch::Checksum::from(0xF7946268),
+      hook                 : nusion_lib::hook!("
+         // Store volatiles and align stack
+         push  rax
 
-      // Call the hook
-      call  {target}
+         // Call the hook
+         call  {target}
 
-      // Restore stack and volatiles
-      pop   rax
+         // Restore stack and volatiles
+         pop   rax
 
-      // Execute stolen bytes and return
-      mov   [rsp+0x60],rax
-      mov   rcx,[rax]
-      mov   [rsp+0x68],rcx
-      mov   rcx,[rax+0x10]
-      ret
-      ", || {
-         // Don't block while waiting for the
-         // lock, this increases the chance of
-         // a race condition
-         let mut lock = match LOOP_RETURN_CODE.try_lock() {
-            Ok(lock) => lock,
-            Err(_)   => return,
-         };
+         // Execute stolen bytes and return
+         mov   [rsp+0x60],rax
+         mov   rcx,[rax]
+         mov   [rsp+0x68],rcx
+         mov   rcx,[rax+0x10]
+         ret
+         ", || {
+            // Don't block while waiting for the
+            // lock, this increases the chance of
+            // a race condition
+            let mut lock = match LOOP_STATUS.try_lock() {
+               Ok(lock) => lock,
+               Err(_)   => return,
+            };
 
-         // If we aren't supposed to execute,
-         // return early
-         if lock.should_execute == false {
+            // If we aren't supposed to execute,
+            // return early to prevent execution
+            // after we are supposed to exit
+            if lock.should_execute == false {
+               return;
+            }
+
+            // Execute the main loop, unwraping the
+            // return code for the loop status struct
+            let should_execute   : bool;
+            let err_code         : Option<Box<dyn std::error::Error + Send>>;
+            match lock.main_loop.as_mut().expect(
+               "Attempted to execute main loop before initialization, this is a bug!",
+            ).execute() {
+               Ok(state) => {
+                  should_execute = state;
+                  err_code       = None;
+               },
+               Err(err) => {
+                  should_execute = false;
+                  err_code       = Some(err);
+               },
+            }
+         
+            // Store the unwrapped error code
+            // in the mutex
+            lock.should_execute  = should_execute;
+            lock.err_code        = err_code;
+
+            // Return from the hook
             return;
          }
+      )
+   };
 
-         // Execute the main loop, unwraping the
-         // return code for the loop status struct
-         let should_execute   : bool;
-         let err_code         : Option<Box<dyn std::error::Error + Send>>;
-         match crate::exec::main_loop(unsafe{& mut LOOP_STATE}) {
-            Ok(state) => {
-               should_execute = state;
-               err_code       = None;
-            },
-            Err(err) => {
-               should_execute = false;
-               err_code       = Some(err);
-            },
-         }
-         
-         // Store the unwrapped error code
-         // in the mutex
-         *lock = LoopStatus{
-            should_execute : should_execute,
-            err_code       : err_code,
-         };
+///////////////////////
+// NUSION ENTRYPOINT //
+///////////////////////
 
-         return;
-      }
-   )
-};
-
-/// Nusion entrypoint.  Keep in mind this executes
+/// Main nusion entrypoint.  Keep in mind this executes
 /// on a separate thread to the main game thread, so
 /// we need synchronization in order to safely read
 /// and write between the main loop hook and this.
@@ -115,17 +123,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
    // main loop.  This currently has a race condition
    // because we might be executing this bit of code
    // while writing, but don't worry about it!
-   let hook_loop = unsafe{crate::game_mut!().patch_create(&HOOK_LOOP)}?;
+   let hook_loop = unsafe{crate::game_mut!().patch_create(&LOOP_HOOK)}?;
 
    // Wait for us to either receive an Ok(false) or Err(_)
    // from the main loop
-   let loop_code : Result<(), Box<dyn std::error::Error>>;
+   let loop_status : Result<(), Box<dyn std::error::Error>>;
    'main_loop : loop {
       // We don't need to check constantly
       std::thread::sleep(std::time::Duration::from_secs(1));
 
       // We don't want to block the thread waiting for the lock
-      let mut lock = match LOOP_RETURN_CODE.try_lock() {
+      let mut lock = match LOOP_STATUS.try_lock() {
          Ok(lock) => lock,
          Err(_)   => continue,
       };
@@ -134,23 +142,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       let should_execute   = lock.should_execute;
       let err_code         = lock.err_code.take();
       
-      // Act upon the state
+      // Decide if we should keep looping and
+      // set the return code accordingly
       if let Some(err_code) = err_code {
-         loop_code = Err(err_code);
+         loop_status = Err(err_code);
+         std::mem::drop(lock.main_loop.take());
          break 'main_loop;
       }
 
       if should_execute == false {
-         loop_code = Ok(());
+         loop_status = Ok(());
+         std::mem::drop(lock.main_loop.take());
          break 'main_loop;
       }
    }
 
    // Explicitly drop the hook container to make
-   // obvious where it is restored
+   // obvious where the game's old code is restored
    std::mem::drop(hook_loop);
 
-   // Return the loop's code
-   return loop_code;
+   // Return the code we got from the main loop
+   return loop_status;
 }
 
